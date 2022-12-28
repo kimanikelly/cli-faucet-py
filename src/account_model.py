f66214aec2f608940300d4acadd5299131af2d7b
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-12-28 16:29:45 -0500

    Configured the Account class

--- src/account_model.py
+++ src/account_model.py
@@ -1,5 +1,24 @@
 class Account:
 
-    def __init__(self, address: str):
+    def __init__(self, account_address: str, contract_address: str, token_name: str, token_symbol: str, amount: int):
 
-        self.address = address
+        self.account_address = account_address
+        self.contract_address = contract_address
+        self.token_name = token_name
+        self.token_symbol = token_symbol
+        self.amount = amount
+
+    def fetch_account_address(self):
+        return self.account_address
+
+    def fetch_contract_address(self):
+        return self.contract_address
+
+    def fetch_token_name(self):
+        return self.token_name
+
+    def fetch_token_symbol(self):
+        return self.token_symbol
+
+    def fetch_amount(self):
+        return self.amount
--- test/account_test.py
+++ test/account_test.py
@@ -4,4 +4,4 @@ from src.account_model import Account
 def test_account():
     testing = Account("xyz")
 
-    assert(testing.address == "xyz")
+    assert(testing.account_address == "xyz")
