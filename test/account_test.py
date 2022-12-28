66ad857531faaa4673f30ff8b9010797f73c155c
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-12-28 18:04:53 -0500

    Verified the that installed smart-contracts-sdk package can be imported into another repo

--- test/account_test.py
+++ test/account_test.py
@@ -1,7 +1,27 @@
-from src.account_model import Account
+import pytest
+
+from src.token_erc20 import Token
+from src.deploy import *
+from src.local_addresses import *
+
+mint_amount = 100
+fund_amount = 100
 
 
-def test_account():
-    testing = Account("xyz")
+@pytest.fixture
+def token():
 
-    assert(testing.account_address == "xyz")
+    addresses["token_local_address"] = deploy_token().address
+
+    return Token(w3.eth.default_account, 1337, "http://localhost:8545")
+
+
+def testing(token):
+
+    token.mint(mint_amount)
+
+    token.set_fund_amount(fund_amount)
+
+    token.fund_account()
+
+    print(token.fetch_balance_of(w3.eth.default_account))
