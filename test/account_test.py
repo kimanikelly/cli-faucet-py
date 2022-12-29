d2401e0acb0b777ee4fb45865ef841b49ccf3507
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-12-28 20:23:20 -0500

    Verified the Account class can store and return data

--- test/account_test.py
+++ test/account_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+from app.account_model import *
 from src.token_erc20 import Token
 from src.deploy import *
 from src.local_addresses import *
@@ -16,7 +17,7 @@ def token():
     return Token(w3.eth.default_account, 1337, "http://localhost:8545")
 
 
-def testing(token):
+def test_account_class(token):
 
     token.mint(mint_amount)
 
@@ -24,4 +25,8 @@ def testing(token):
 
     token.fund_account()
 
-    print(token.fetch_balance_of(w3.eth.default_account))
+    user_account = Account(w3.eth.default_account, token.address, mint_amount)
+
+    assert(user_account.fetch_account_address() == w3.eth.default_account)
+    assert(user_account.fetch_contract_address() == token.address)
+    assert(user_account.fetch_amount() == mint_amount)
