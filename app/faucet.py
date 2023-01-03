2e18b52164c510a5749bc717d3c0ccce149e4f72
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2023-01-03 15:34:39 -0500

    CLI Comands Implemented the contract-balance command Refactored start.sh to commands.sh

--- app/faucet.py
+++ app/faucet.py
@@ -21,11 +21,18 @@ def cli():
     pass
 
 
-@cli.command(help="The amount of Test Tokens transferred to a recipient's wallet")
+@cli.command(help="The amount of Test Tokens transferred to a wallet")
 def fund_amount():
 
     click.echo(
-        f"The fund amount is: {w3.fromWei(token.fetch_fund_amount(), 'ether')} TT ")
+        f"The fund amount is: {w3.fromWei(token.fetch_fund_amount(), 'ether')} TT")
+
+
+@cli.command(help="View the amount of Test Tokens Token.sol holds")
+def contract_balance():
+
+    click.echo(
+        f"The contract balance is: {w3.fromWei(token.fetch_balance_of(token.address),'ether')} TT")
 
 
 if __name__ == '__main__':
